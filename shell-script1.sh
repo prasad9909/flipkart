478bfbc180fc0f95320c489f95bf5d66c76b6313
Siva <siva6015@gmail.com>
2025-01-20 15:46:08 +0530

    conversation

--- shell-script1.sh
+++ shell-script1.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo "Rahinm:: Hi Ram"
-echo "Ram:: Hello Rahim"
-echo "Rahim:: how are you"
-echo "Ram:: I'm fine"
+Person1=Rahim
+Person2=Ram
+
+echo "Person1:: Hi Ram"
+echo "Person2:: Hello Rahim"
+echo "Person1:: how are you"
+echo "Person2:: I'm fine"
 
